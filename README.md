a07257a10e5aa57de157f8e8dd7c56ee5b3e1181
Sebastian Wilzbach <seb@wilzba.ch>
2015-07-10 00:13:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+BioJS web component dependency example
+======================================
+
+An example of a web component using dependencies, especially
+
+ - [`bio-element`](https://github.com/biojs/bio-element).
+ - [`bio-container`](https://github.com/biojs/bio-container).
+
+Install
+-------
+
+```
+jspm i
+```
+
+Description
+-----------
+
+The dependencies are injected by using the [SystemJS module loader](https://github.com/systemjs/systemjs) and managed with [jspm](http://jspm.io).
+See [BioJS3](https://github.com/biojs/biojs3) for more details.
+
+Contributors
+------------
+
+A huge thanks to all these amazing contributors
+
+ - [@Hypercubed](https://github.com/Hypercubed)
+ - [@herkulano](https://github.com/herkunalo)
+ - [@greenify](https://github.com/greenify)
---
+++ config.js
@@ -0,0 +1,44 @@
+System.config({
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.6.17",
+    "babel-runtime": "npm:babel-runtime@5.6.17",
+    "bio-container": "github:biojs/bio-container@0.0.3",
+    "bio-element": "github:biojs/bio-element@0.0.1",
+    "core-js": "npm:core-js@0.9.18",
+    "css": "github:systemjs/plugin-css@0.1.13",
+    "html": "github:Hypercubed/systemjs-plugin-html@master",
+    "lodash": "github:lodash/lodash@3.10.0",
+    "polymer": "github:greenify/polymer-js@1.0.5",
+    "text": "github:systemjs/plugin-text@0.0.2",
+    "webcomponentsjs-lite": "github:webcomponents/webcomponentsjs@0.7.2",
+    "github:biojs/bio-container@0.0.3": {
+      "bio-element": "github:biojs/bio-element@0.0.1"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.6.17": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
+  }
+});
+
