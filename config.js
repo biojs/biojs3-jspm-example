d3537e3907f09e1f43da0868d1986ac2772b85f3
Herculano Campos <herkulano@gmail.com>
2015-07-10 15:34:39 +0100

    change in dependencies

--- config.js
+++ config.js
@@ -16,17 +16,18 @@ System.config({
   "map": {
     "babel": "npm:babel-core@5.6.17",
     "babel-runtime": "npm:babel-runtime@5.6.17",
-    "bio-container": "github:biojs/bio-container@0.0.4",
-    "bio-element": "github:biojs/bio-element@0.0.2",
+    "bio-container": "github:herkulano/bio-container@0.0.5",
     "core-js": "npm:core-js@0.9.18",
-    "css": "github:systemjs/plugin-css@0.1.13",
-    "html": "github:Hypercubed/systemjs-plugin-html@master",
-    "lodash": "github:lodash/lodash@3.10.0",
-    "polymer": "github:greenify/polymer-js@1.0.5",
-    "text": "github:systemjs/plugin-text@0.0.2",
-    "webcomponentsjs-lite": "github:webcomponents/webcomponentsjs@0.7.2",
-    "github:biojs/bio-container@0.0.4": {
-      "bio-element": "github:biojs/bio-element@0.0.2"
+    "github:herkulano/bio-container@0.0.5": {
+      "bio-element": "github:herkulano/bio-element@0.0.5",
+      "html": "github:Hypercubed/systemjs-plugin-html@master",
+      "polymer": "github:greenify/polymer-js@1.0.5",
+      "webcomponentsjs-lite": "github:webcomponents/webcomponentsjs@0.7.2"
+    },
+    "github:herkulano/bio-element@0.0.5": {
+      "html": "github:Hypercubed/systemjs-plugin-html@master",
+      "polymer": "github:greenify/polymer-js@1.0.5",
+      "webcomponentsjs-lite": "github:webcomponents/webcomponentsjs@0.7.2"
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
